d27de2343fa077045a21429e381b2d6e96f2a674
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-20 17:29:13 -0300

    Initial Commit

---
+++ StringAnimation/StringAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringAnimationApp.swift
+//  StringAnimation
+//
+//  Created by Orlando Moraes Martins on 20/12/22.
+//
+
+import SwiftUI
+
+@main
+struct StringAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
